2ab871ed288bdd1de7118a73b02ffc912bdb143d
Vinay Kanigicherla <vinaykanigicherla@berkeley.edu>
2021-01-15 23:44:06 -0800

    minor fixes

--- pdftolatex/segment_pdf.py
+++ pdftolatex/segment_pdf.py
@@ -17,22 +17,22 @@ def segment(img):
     
     blur = cv2.GaussianBlur(img_bw, (7,7), 0) 
     
-    simple_plot(blur)
+    #simple_plot(blur)
 
     k1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
     m1 = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, k1)
     
-    simple_plot(m1)
+    #simple_plot(m1)
     
     k2 = cv2.getStructuringElement(cv2.MORPH_RECT, (HORIZONTAL_POOLING, 5))
     m2 = cv2.morphologyEx(m1, cv2.MORPH_CLOSE, k2)
 
-    simple_plot(m2)    
+    #simple_plot(m2)    
 
     k3 = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
     m3 = cv2.dilate(m2, k3, iterations=2)
     
-    simple_plot(m3)
+    #simple_plot(m3)
 
     contours = cv2.findContours(m3, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
